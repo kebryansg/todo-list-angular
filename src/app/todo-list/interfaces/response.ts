55dd98398e506684f72869f0bd3b66ee5f0bab9a
jlsaltos <jlsaltos@pichincha.com>
2022-06-23 18:22:01 -0500

    proyecto vacio

--- src/app/todo-list/interfaces/response.ts
+++
@@ -1,7 +0,0 @@
-import { Todo } from "./todo";
-
-export interface ResponseTodo {
-    success: boolean;
-    type:string;
-    data: Todo[]
-}
--- src/app/todo-list/interfaces/todo.ts
+++
@@ -1,8 +0,0 @@
-export interface Todo {
-    id?: number;
-    id_author: number;
-    status: number;
-    description: string;
-    finish_at: string;
-    created_at?: string;
-}
